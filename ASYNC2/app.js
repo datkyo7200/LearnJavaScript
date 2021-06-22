a044c7028a8b548e26882082a6383915c85f0405
datkyo7200 <dat.phan@bigfootprintdigital.com>
2021-06-22 08:45:41 +0700

    update

--- ASYNC2/app.js
+++ ASYNC2/app.js
@@ -1,60 +1,64 @@
 // Sync code example
 
 // const otherFunct = () => {
-//     console.log('we are in another function');
-//     console.log('do some stuff');
-// }
-
-// console.log('Start');
-
-// otherFunct();
-
-// console.log('End');
-
-// Async code example
+//     console.log("we are in another function");
+//     console.log("do some stuff");
+// };
 
 // console.log("Start");
 
-// setTimeout(() => {
-//   console.log("We are in the timeout");
-// }, 5000);
+// otherFunct();
 
 // console.log("End");
 
+// Async code example
+
 console.log("Start");
 
-const loginUser = (email, password, callback) => {
-  setTimeout(() => {
-    console.log("Now we have the data");
-    callback({ userEmail: email });
-  }, 3000);
-};
+setTimeout(() => {
+    console.log("We are in the timeout 3s");
+}, 3000);
 
-const getUserVideos = (email, callback) => {
-  setTimeout(() => {
-    callback([
-      { id: "video1", title: "title of video 1" },
-      { id: "video2", title: "title of video 2" },
-      { id: "video3", title: "title of video 3" },
-      { id: "video4", title: "title of video 4" },
-    ]);
-  }, 1000);
-};
-
-const videoDetails = (video, callback) => {
-  setTimeout(() => {
-    callback(video);
-  }, 2000);
-};
-
-const user = loginUser("datkyo7200@gmail.com", "12345678", (user) => {
-  console.log(user);
-  getUserVideos(user.userEmail, (videos) => {
-    console.log(videos);
-    videoDetails(videos[0].title, (title) => {
-      console.log(title);
-    });
-  });
-});
+setTimeout(() => {
+    console.log("We are in the timeout 2s");
+}, 2000);
 
 console.log("End");
+
+// console.log("Start");
+
+// const loginUser = (email, password, callback) => {
+//   setTimeout(() => {
+//     console.log("Now we have the data");
+//     callback({ userEmail: email });
+//   }, 3000);
+// };
+
+// const getUserVideos = (email, callback) => {
+//   setTimeout(() => {
+//     callback([
+//       { id: "video1", title: "title of video 1" },
+//       { id: "video2", title: "title of video 2" },
+//       { id: "video3", title: "title of video 3" },
+//       { id: "video4", title: "title of video 4" },
+//     ]);
+//   }, 1000);
+// };
+
+// const videoDetails = (video, callback) => {
+//   setTimeout(() => {
+//     callback(video);
+//   }, 2000);
+// };
+
+// const user = loginUser("datkyo7200@gmail.com", "12345678", (user) => {
+//   console.log(user);
+//   getUserVideos(user.userEmail, (videos) => {
+//     console.log(videos);
+//     videoDetails(videos[0].title, (title) => {
+//       console.log(title);
+//     });
+//   });
+// });
+
+// console.log("End");
--- ASYNC2/callback.js
+++ ASYNC2/callback.js
@@ -2,59 +2,43 @@
 
 // const otherFunct = () => {
 //     console.log('we are in another function');
-//     console.log('do some stuff');
-// }
 
-// console.log('Start');
-
-// otherFunct();
-
-// console.log('End');
-
-// Async code example
-
-// console.log("Start");
-
-// setTimeout(() => {
-//   console.log("We are in the timeout");
-// }, 5000);
-
-// console.log("End");
+// callbacks in asynchronus
 
 console.log("Start");
 
 const loginUser = (email, password, callback) => {
-  setTimeout(() => {
-    console.log("Now we have the data");
-    callback({ userEmail: email });
-  }, 3000);
+    setTimeout(() => {
+        console.log("Now we have the data");
+        callback({ userEmail: email });
+    }, 3000);
 };
 
 const getUserVideos = (email, callback) => {
-  setTimeout(() => {
-    callback([
-      { id: "video1", title: "title of video 1" },
-      { id: "video2", title: "title of video 2" },
-      { id: "video3", title: "title of video 3" },
-      { id: "video4", title: "title of video 4" },
-    ]);
-  }, 1000);
+    setTimeout(() => {
+        callback([
+            { id: "video1", title: "title of video 1" },
+            { id: "video2", title: "title of video 2" },
+            { id: "video3", title: "title of video 3" },
+            { id: "video4", title: "title of video 4" },
+        ]);
+    }, 1000);
 };
 
 const videoDetails = (video, callback) => {
-  setTimeout(() => {
-    callback(video);
-  }, 2000);
+    setTimeout(() => {
+        callback(video);
+    }, 2000);
 };
 
 const user = loginUser("datkyo7200@gmail.com", "12345678", (user) => {
-  console.log(user);
-  getUserVideos(user.userEmail, (videos) => {
-    console.log(videos);
-    videoDetails(videos[0].title, (title) => {
-      console.log(title);
+    console.log(user);
+    getUserVideos(user.userEmail, (videos) => {
+        console.log(videos);
+        videoDetails(videos[0].title, (title) => {
+            console.log(title);
+        });
     });
-  });
 });
 
 console.log("End");
